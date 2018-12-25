67d3ea38160583cc58be9ee7b6852122df59bba3
phuonganh313 <anhdtp@hamsa.vn>
2018-12-25 13:48:34 +0700

    ST2-Built strct left setting page

--- resources/lang/en/label.php
+++ resources/lang/en/label.php
@@ -3,5 +3,18 @@
 return [
     'setup'=> 'Setup',
     'update_successfully' => 'Update successfully',
-    'un_successfully' => 'Unsuccessfully'
+    'un_successfully' => 'Unsuccessfully',
+    'add_to_cart' => 'Add to cart',
+    'title' => 'Title',
+    'product_name' => 'Product Name',
+    'total_amount' => 'Total Amount',
+    'new_price' => 'New Price',
+    'old_price' => 'Old Price',
+    'translation' => 'Tanslation',
+    'display' => 'Display',
+    'design_and_support_by_hamsa_technology' => 'Design and support by Hamsa Technology',
+    'user_guide' => 'User guide',
+    'help' => 'Help',
+    'save' => 'Save',
+    'preview'=> 'Preview',
 ];
