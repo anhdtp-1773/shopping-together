f124416f19e7ac8b3f81f3e30c93c10c89c0ac3c
PhuongNamdz <namdv@hamsa.vn>
2018-12-25 15:44:51 +0700

    save cart rule

--- app/CartRule.php
+++ app/CartRule.php
@@ -13,4 +13,19 @@ class CartRule extends Model
      */
     protected $table = 'cart_rule';
     protected $fillable=['id_shop','name','status','updated_at','created_at'];
+
+    public static function saveCartRule($id_shop, $name, $status){
+        $cart_rule = new CartRule();
+        $cart_rule->id_shop = $id_shop;
+        $cart_rule->name = $name;
+        $cart_rule->status = $status;
+        $cart_rule->save();
+        return $cart_rule;
+    }
+
+    public static function saveCartRuleDetail($array_products)
+    {
+        DB::table('cart_rule_detail')->insert($array_products);
+    }
+
 }
---
+++ app/Http/Controllers/CartRuleController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+use App\Variant;
+use App\Image;
+use App\Shop;
+use App\CartRule;
+
+class CartRuleController extends Controller
+{
+    public function save(Request $request)
+    {
+        $errors = array();
+        $validator = \Validator::make($request->all(), [
+            'name' => 'required',
+        ]);
+
+        if($validator->fails()){
+            $errors = $validator->errors();
+        }
+
+        if(!$errors){
+            $shop_info = Shop::getShopByDomain($request->shopify_domain);
+            if($shop_info){
+                $cart_rule = CartRule::saveCartRule(
+                    $shop_info->id,
+                    $request->name,
+                    $request->status
+                );
+                if($cart_rule){
+                    $array_products = array();
+                    if($request->products){
+                        foreach($request->products as $product){
+                            $array_products[] = array(
+                                'id_cart_rule' => $cart_rule->id,
+                                'id_shop' => $shop_info->id,
+                                'id_main_product' => isset($product->id_main_product) ? $product->id_main_product : '',
+                                'id_related_product' => isset($product->id_related_product) ? $product->id_related_product : '',
+                                'reduction_percent' => isset($product->reduction_percent),
+                                'reduction_amount' => isset($product->reduction_amount)
+                            );
+                        }
+                        CartRule::saveCartRuleDetail($array_products);
+                    }
+                }
+            }
+        }
+
+        return response()->json([
+            'message' => $errors ? $errors: trans('label.update_successfully'),
+            'status' => $errors ? false : true,
+        ], 200); 
+    }
+}
