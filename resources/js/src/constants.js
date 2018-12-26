834da379e87636c1d2da60ee61d54468ac3b54b3
phuonganh313 <anhdtp@hamsa.vn>
2018-12-26 10:52:25 +0700

    add constants file

---
+++ resources/js/src/constants.js
@@ -0,0 +1,16 @@
+export const optionsFontFamily = [
+    {label: 'Verdana', value: 'verdana'},
+    {label: 'Arial', value: 'arial'},
+    {label: 'Times New Roman', value: 'timesNewRoman'},
+    {label: 'Segoe UI', value: 'segoeUI'},
+];
+
+export const optionsFontStyle = [
+    {label: 'Normal', value: 'normal'},
+    {label: 'Italic', value: 'italic'},
+    {label: 'Bold', value: 'bold'},
+];
+export const optionsSize = [
+    {label: 'Size-3, Color-S', value: 'Size-3, Color-S',
+    }
+]
