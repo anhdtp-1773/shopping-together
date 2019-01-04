60bffd4dfe35d3ef46b8886ff7519ffd64104c1b
ngminhtan <losblancos893@gmail.com>
2019-01-04 10:34:22 +0800

    remove dubcode

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -20,7 +20,6 @@ class ProductController extends Controller
         $msg = trans('label.successfully');
         try{
             $data = Product::getProducts($this->page_number, $this->items_per_page);
-            dd($data);
         }
         catch(\Exception $e){
             $status = false;
