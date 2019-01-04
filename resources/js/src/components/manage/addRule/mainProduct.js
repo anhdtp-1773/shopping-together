fd53a6ed1bb7c6672a0aa18bafc8214c907c0601
PhuongNamdz <namdv@hamsa.vn>
2019-01-04 09:12:07 +0700

    remove data not use

--- resources/js/src/components/manage/addRule/mainProduct.js
+++ resources/js/src/components/manage/addRule/mainProduct.js
@@ -34,7 +34,7 @@ export default class MainProduct extends Component {
     }
 
     render() {
-        const {currentPage, itemsPerPage, totalItems, products, isSearchProduct, msg, ruleName, requiredFields, validates, keyWord} = this.props;
+        const {currentPage, itemsPerPage, totalItems, products, msg, ruleName, requiredFields, validates, keyWord} = this.props;
         const disabledOnClick =  _.isEmpty(requiredFields) ? true : !_.every(_.values(validates), function(value) {return value == 'valid'}); 
         return (
             <div className="container">
@@ -84,29 +84,6 @@ export default class MainProduct extends Component {
                         <p>{msg}</p>
                 }
                 
-                {
-                    isSearchProduct
-                    ?
-                    null
-                    :
-                    <Fragment>
-                        <Pagination
-                            activePage={currentPage}
-                            itemsCountPerPage={itemsPerPage}
-                            totalItemsCount={totalItems}
-                            pageRangeDisplayed={5}
-                            onChange={this.props.handlePageChange}
-                        />
-                        <button 
-                            onClick={this.nextStep.bind(this, 2)} 
-                            type="button" 
-                            className={classNames({'btn btn-primary': true}, {'disabled-form': disabledOnClick})}
-                            style={{float:"right"}}
-                        >
-                            {lang.next}
-                        </button>
-                    </Fragment> 
-                }
                 <Fragment>
                     <Pagination
                         activePage={currentPage}
@@ -115,7 +92,14 @@ export default class MainProduct extends Component {
                         pageRangeDisplayed={5}
                         onChange={this.props.handlePageChange}
                     />
-                    <button onClick={this.nextStep.bind(this, 2)} type="button" class="btn btn-primary" style={{float:"right"}}>{lang.next}</button>
+                    <button 
+                        onClick={this.nextStep.bind(this, 2)} 
+                        type="button" 
+                        className={classNames({'btn btn-primary': true}, {'disabled-form': disabledOnClick})}
+                        style={{float:"right"}}
+                    >
+                        {lang.next}
+                    </button>
                 </Fragment> 
             </div>
         );
