11088ba2177a7e66592b034baefc327fa7fa4324
PhuongNamdz <namdv@hamsa.vn>
2019-01-21 23:44:28 +0700

    add file bundle

--- resources/js/src/components/manage/addRule/addRule.js
+++ resources/js/src/components/manage/addRule/addRule.js
@@ -206,6 +206,7 @@ export default class AddRule extends Component {
                         }
                     </div>
                     <div>
+                        
                         {
                             step == 2
                             ?
