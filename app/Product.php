0223fb38777735039a463bfe39a9facc3709290e
PhuongNamdz <namdv@hamsa.vn>
2019-02-12 09:53:47 +0700

    optimize code

--- app/Product.php
+++ app/Product.php
@@ -128,9 +128,8 @@ class Product extends Model
         return DB::table('products')->where('id_shopify_product', $id_shopify_product)
                     ->join('images', 'products.id_shopify_product', '=', 'images.id_product')
                     ->join('variants', 'products.id_shopify_product', '=', 'variants.id_product')
-                    ->join('currency', 'products.id_shop', '=', 'currency.id_shop')
                     ->select('products.title','products.id_shopify_product', 'images.src', 'variants.price', 'variants.option1', 
-                            'variants.option2', 'variants.option3', 'currency.currency')
+                            'variants.option2', 'variants.option3')
                     ->first();
     }
     
